028beed2492f665c0792989590bbf6654dc0d41a
Matteo Gardusi <matteo.gardusi@gmail.com>
2022-12-10 21:17:01 +0100

    First commit

---
+++ custom_components/zcsazzurro/sensor.py
@@ -0,0 +1,257 @@
+"""Support for getting collected information from ZCSAzzurro."""
+from __future__ import annotations
+
+from datetime import timedelta
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components.rest.data import RestData
+from homeassistant.components.sensor import (
+    DEVICE_CLASS_ENERGY,
+    PLATFORM_SCHEMA,
+    STATE_CLASS_MEASUREMENT,
+    SensorEntity,
+)
+from homeassistant.const import (
+    ATTR_DATE,
+    ATTR_TEMPERATURE,
+    ATTR_TIME,
+    CONF_NAME,
+    ENERGY_WATT_HOUR,
+)
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+
+import json
+
+_LOGGER = logging.getLogger(__name__)
+_ENDPOINT = "https://third.zcsazzurroportal.com:19003"
+
+CONF_THINGKEY = "thingkey"
+CONF_AUTHKEY = "authkey"
+CONF_CLIENTCODE = "clientcode"
+
+DEFAULT_NAME = "ZCSAzzurro"
+DEFAULT_VERIFY_SSL = False
+
+SCAN_INTERVAL = timedelta(minutes=2)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_THINGKEY): cv.string,
+        vol.Required(CONF_AUTHKEY): cv.string,
+        vol.Required(CONF_CLIENTCODE): cv.string,
+        vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    }
+)
+
+
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Set up the ZCSAzzurro sensor."""
+    name = config.get(CONF_NAME)
+    client_code = config.get(CONF_CLIENTCODE)
+    auth_key = config.get(CONF_AUTHKEY)
+    thing_key = config.get(CONF_THINGKEY)
+    method = "POST"
+    auth = None
+    verify_ssl = DEFAULT_VERIFY_SSL
+    timeout = 69
+
+    payload = { 
+         "realtimeData":  { 
+              "command": "realtimeData", 
+              "params": { 
+                  "thingKey": thing_key,
+                  "requiredValues": "*"
+            }
+        }
+    }
+    string_payload = json.dumps(payload)
+    
+    headers = {
+        "Client": client_code,
+        "Authorization": auth_key
+    }
+
+    _LOGGER.debug("method: %s", method)
+    _LOGGER.debug("_ENDPOINT: %s", _ENDPOINT)
+    _LOGGER.debug("auth: %s", auth)
+    _LOGGER.debug("headers: %s", headers)
+    _LOGGER.debug("payload: %s", string_payload)
+    _LOGGER.debug("verify_ssl: %s", verify_ssl)
+    
+    rest = RestData(hass, method, _ENDPOINT, auth, headers, None, string_payload, verify_ssl, timeout)
+    await rest.async_update()
+
+    if rest.data is None:
+        _LOGGER.error("Unable to start fetching data from ZCSAzzurro")
+        # return False
+
+    async_add_entities([DSSoutputSensor(rest, name, thing_key)])
+
+class DSSoutputSensor(SensorEntity):
+    """Representation of a ZCSAzzurro sensor."""
+
+    _attr_state_class = STATE_CLASS_MEASUREMENT
+    _attr_device_class = DEVICE_CLASS_ENERGY
+    _attr_native_unit_of_measurement = ENERGY_WATT_HOUR
+
+    def __init__(self, rest, name, thing_key):
+        """Initialize a ZCSAzzurro sensor."""
+        self.rest = rest
+        self._attr_name = name
+        self._attributes = None
+        self.dssoutput = None
+        self._state = False
+        self._thing_key = thing_key
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        value = self._state
+        _LOGGER.debug("Return the state: %s", value)
+        return value
+
+
+    @property
+    def extra_state_attributes(self):
+        """Return the state attributes of the monitored installation."""
+
+        if self.dssoutput is not None:
+            thing_key = self._thing_key
+            _LOGGER.debug("Return the state attributes")
+            _LOGGER.debug("thingkey: %s", thing_key)
+            
+            try: lastUpdated = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["lastUpdate"]
+            except: lastUpdated = 0
+             
+            try: energyGeneratingTotal = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyGeneratingTotal"]
+            except: energyGeneratingTotal = 0
+
+            try: energyChargingTotal = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyChargingTotal"]
+            except: energyChargingTotal = 0
+
+            try: energyDischargingTotal = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyDischargingTotal"]
+            except: energyDischargingTotal = 0
+
+            try: energyExportingTotal = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyExportingTotal"]
+            except: energyExportingTotal = 0
+
+            try: energyImportingTotal = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyImportingTotal"]
+            except: energyImportingTotal = 0
+
+            try: energyConsumingTotal = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyConsumingTotal"]
+            except: energyConsumingTotal = 0
+
+            try: energyAutoconsumingTotal = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyAutoconsumingTotal"]
+            except: energyAutoconsumingTotal = 0
+
+            try: energyGenerating = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyGenerating"]
+            except: energyGenerating = 0
+
+            try: powerGenerating = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["powerGenerating"]
+            except: powerGenerating = 0
+
+            try: batteryCycletime = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["batteryCycletime"]
+            except: batteryCycletime = 0
+
+            try: batterySoC = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["batterySoC"]
+            except: batterySoC = 0
+
+            try: powerCharging = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["powerCharging"]
+            except: powerCharging = 0
+
+            try: powerDischarging = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["powerDischarging"]
+            except: powerDischarging = 0
+
+            try: powerExporting = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["powerExporting"]
+            except: powerExporting = 0
+
+            try: powerImporting = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["powerImporting"]
+            except: powerImporting = 0
+
+            try: powerConsuming = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["powerConsuming"]
+            except: powerConsuming = 0
+
+            try: powerAutoconsuming = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["powerAutoconsuming"]
+            except: powerAutoconsuming = 0
+
+            try: energyCharging = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyCharging"]
+            except: energyCharging = 0
+
+            try: energyDischarging = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyDischarging"]
+            except: energyDischarging = 0
+
+            try: energyExporting = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyExporting"]
+            except: energyExporting = 0
+
+            try: energyImporting = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyImporting"]
+            except: energyImporting = 0
+
+            try: energyConsuming = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyConsuming"]
+            except: energyConsuming = 0
+
+            try: energyAutoconsuming = self.dssoutput["realtimeData"]["params"]["value"][0][thing_key]["energyAutoconsuming"]
+            except: energyAutoconsuming = 0
+
+            return {
+                "lastUpdated": lastUpdated,
+                "thing.find": thing_key,
+                "total": {
+                    "energyGenerating": energyGeneratingTotal,
+                    "energyCharging": energyChargingTotal,
+                    "energyDischarging": energyDischargingTotal,
+                    "energyExporting": energyExportingTotal,
+                    "energyImporting": energyImportingTotal,
+                    "energyConsuming": energyConsumingTotal,
+                    "energyAutoconsuming": energyAutoconsumingTotal,
+                },
+                "current": {
+                    "energyGenerating": energyGenerating,
+                    "powerGenerating": powerGenerating,
+                    "batteryCycletime": batteryCycletime,
+                    "batterySoC": batterySoC,
+                    "powerCharging": powerCharging,
+                    "powerDischarging": powerDischarging,
+                    "powerExporting": powerExporting,
+                    "powerImporting": powerImporting,
+                    "powerConsuming": powerConsuming,
+                    "powerAutoconsuming": powerAutoconsuming,
+                    "energyCharging": energyCharging,
+                    "energyDischarging": energyDischarging,
+                    "energyExporting": energyExporting,
+                    "energyImporting": energyImporting,
+                    "energyConsuming": energyConsuming,
+                    "energyAutoconsuming": energyAutoconsuming,
+                },
+            }
+
+    async def async_update(self):
+        """Get the latest data from the ZCSAzzurro API and updates the state."""
+        await self.rest.async_update()
+        self._async_update_from_rest_data()
+
+    async def async_added_to_hass(self):
+        """Ensure the data from the initial update is reflected in the state."""
+        self._async_update_from_rest_data()
+
+    @callback
+    def _async_update_from_rest_data(self):
+        """Update state from the rest data."""
+        try:
+            json_dict = self.rest.data
+            if json_dict is not None:
+                try:
+                    _LOGGER.debug("Data fetched from resource: %s", json_dict)
+                    json_dict = json.loads(json_dict)
+                    self.dssoutput = json_dict
+                    self._state = self.dssoutput["realtimeData"]["success"] == True
+                except ValueError:
+                    _LOGGER.warning("REST result could not be parsed as JSON")
+                    _LOGGER.debug("Erroneous JSON: %s", json_dict)
+            else:
+                _LOGGER.warning("Empty reply found when expecting JSON data")
+        except TypeError:
+            _LOGGER.error("Unable to fetch data from ZCSAzzurro. Response: %s", self.rest.data)
+
---
+++ info.md
@@ -0,0 +1,216 @@
+# SDeSalve Home Assistant Integration: zcsazzurro
+
+[![Buy me a coffee][buymeacoffee-shield]][buymeacoffee] [![Support my work on Paypal][paypal-shield]][paypal]
+
+This integration allows you to polling your ZCS Azzurro inverter's realtime data from API.
+
+Now support the new ZCS API.
+
+## Usage:
+Obtain your Device Serial/ThingKey and your Authorization header value from ZCS Zucchetti.
+
+Add to configuration.yaml:
+
+```
+sensor:
+  - platform: zcsazzurro
+    name: fotovoltaico
+    thingkey: [YOUR DEVICE SERIAL - THINGKEY]
+    authkey: [YOUR AUTHORIZATION HEADER VALUE]
+    clientcode: [YOUR CLIENT CODE HERE]    
+    
+```
+
+## Example for extract sensor
+```
+sensor:
+  - platform: integration
+    name: power_generating_spent_zcs
+    source: sensor.power_generating_zcs
+    unit_prefix: k
+    round: 2    
+    method: left
+    
+##### current   
+template:
+  - sensor:
+      - name: "Potenza Istantanea"
+        unit_of_measurement: "W"
+        state: >
+          {% set power = state_attr('sensor.fotovoltaico','current')['powerGenerating'] | int(0) %}
+          {{ power }}
+        state_class: measurement
+        device_class: power
+        icon: mdi:solar-power
+
+      - name: "Batteria"
+        unit_of_measurement: "%"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['batterySoC'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:battery-90
+        
+      - name: "Consumo Giorno Casa"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['energyConsuming'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:power-socket-it  
+        
+      - name: "Autoconsum Giorno"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['energyAutoconsuming'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:power-plug-outline    
+        
+      - name: "Scarica"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['energyDischarging'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:battery-low 
+        
+      - name: "Carica"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['energyCharging'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:battery-high 
+        
+      - name: "Produzione"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['energyGenerating'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:solar-power
+        
+      - name: "Prelievo"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['energyImporting'] | float(0)%}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:transmission-tower  
+        
+      - name: "Immissione"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','current')['energyExporting'] | float (0) %}
+          {{ energy | round(2) }}
+        state_class: measurement
+        device_class: energy
+        icon: mdi:solar-power 
+           
+##### total         
+      - name: "Produzione Totale"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','total')['energyGenerating'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: total_increasing
+        device_class: energy
+        icon: mdi:weather-sunny
+        
+      - name: "Prelievo Totale"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','total')['energyImporting'] | float (0) %}
+          {{ energy | round(2) }}
+        state_class: total_increasing
+        device_class: energy
+        icon: mdi:transmission-tower
+        
+      - name: "Immissione Totale"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','total')['energyExporting'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: total_increasing
+        device_class: energy
+        icon: mdi:flash-circle
+        
+      - name: "Autoconsum Totale"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','total')['energyAutoconsuming'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: total_increasing
+        device_class: energy
+        icon: mdi:power-plug-outline 
+        
+      - name: "Scarica Totale"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','total')['energyDischarging'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: total_increasing
+        device_class: energy
+        icon: mdi:battery-low
+        
+      - name: "Carica Totale"
+        unit_of_measurement: "kWh"
+        state: >
+          {% set energy = state_attr('sensor.fotovoltaico','total')['energyCharging'] | float(0) %}
+          {{ energy | round(2) }}
+        state_class: total_increasing
+        device_class: energy
+        icon: mdi:battery-high
+```
+
+## Example sensor for energy panel
+```
+utility_meter:
+  total_energy_generating_zcs:
+    source: sensor.energy_generating_zcs
+    cycle: daily
+
+  total_energy_exporting_zcs:
+    source: sensor.energy_exporting_zcs
+    cycle: daily
+
+  total_energy_importing_zcs:
+    source: sensor.energy_importing_zcs
+    cycle: daily
+```
+
+## Authors & contributors
+
+The original setup of this repository is by [SDeSalve][sdesalve].
+
+Thanks to Alesoft73 for their support to implement new API.
+
+## License
+
+Copyright (c) 2021 SDeSalve
+
+See [LICENSE][license]
+
+## Trademark legal notice
+
+This integration is not created, developed, affiliated, supported, maintained or endorsed by **Zucchetti Centro Sistemi S.p.A.**.
+All product names, logos, brands, trademarks and registered trademarks are property of their respective owners. All company, product, and service names used are for identification purposes only.
+Use of these names, logos, trademarks, and brands does not imply endorsement.
+
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg)](https://github.com/sdesalve/zcsazzurro)
+
+[buymeacoffee-shield]: https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-2.svg
+[buymeacoffee]: https://www.buymeacoffee.com/sdesalve
+[paypal-shield]: https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif
+[paypal]: https://paypal.me/SDeSalve
+[license]: https://github.com/sdesalve/zcsazzurro/LICENSE.md
+[sdesalve]: https://github.com/sdesalve
